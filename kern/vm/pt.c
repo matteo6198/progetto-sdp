7b6f7acfc9b1ad30f31814b25d0206f1eddb6b29
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2021-08-14 15:44:39 +0200

    updated definition of pt_get_page()

--- kern/vm/pt.c
+++ kern/vm/pt.c
@@ -38,7 +38,7 @@ static int pt_get_victim(void){
 }
 
 /* returns the entry corresponding to the page associated to the address v_addr (if that page is not in memory it will be loaded)*/
-int pt_get_page(vaddr_t v_addr){
+paddr_t pt_get_page(vaddr_t v_addr, uint8_t *flags){
     v_addr &= PAGE_FRAME;/*
     // ricerca nella PT
     int found = 0;
