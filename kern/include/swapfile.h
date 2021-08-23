5136916d1768cfa2f64d30a970fe74f85b43cdbc
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2021-08-23 16:41:40 +0200

    added swapfile.c and swapfile.h

---
+++ kern/include/swapfile.h
@@ -0,0 +1,41 @@
+#ifndef _SWAPFILE_H_
+#define _SWAPFILE_H_
+
+#include <types.h>
+
+#define SWAP_DISCARD    0
+#define SWAP_LOAD       1
+
+#define SWAP_FILESIZE   9*1024*1024
+
+/* swap_bootrsap
+    Alloca le strutture dati utili per la gestione dello
+    SWAPFILE
+ */
+void swap_bootstrap(void);
+
+/*  swap_in
+    vaddr_t v_addr: indirizzo logico della pagina
+                    da caricare in memoria
+    pid_t      pid: pid del processo
+    uint8_t  store: SWAP_DISCARD per scartarla, SWAP_LOAD per
+                    copiarla in memoria
+    Prende la pagina dallo swapfile e la carica in memoria,
+    se non trova la pagina non fa nulla.
+
+    Ritorna 0 se NON trova la pagina nello SWAPFILE, altrimenti
+    ritorna un valore diverso da 0.
+*/
+int swap_in(vaddr_t v_addr, pid_t pid, uint8_t store);
+
+/*  swap_out
+    vaddr_t   v_addr: indirizzo logico della pagina
+                      da inserire nello SWAPFILE
+    pid_t        pid: pid del processo
+
+    Prende una pagina dalla memoria e la inserisce nello SWAPFILE
+    (no return code)
+*/
+void swap_out(vaddr_t v_addr, pid_t pid);
+
+#endif /* _SWAPFILE_H_ */
---
+++ kern/vm/swapfile.c
@@ -0,0 +1,17 @@
+#include <swapfile.h>
+
+
+void swap_bootstrap(void)
+{
+
+}
+
+int swap_in(vaddr_t v_addr, pid_t pid, uint8_t store)
+{
+
+}
+
+void swap_out(vaddr_t v_addr, pid_t pid)
+{
+    return
+}
